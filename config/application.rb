871b5784ed9e54887a0558e09a9ac264f582e08f
alec-petros <a.petros@verizon.net>
2018-05-08 17:03:17 -0400

    lol

--- config/application.rb
+++ config/application.rb
@@ -4,8 +4,6 @@ require 'rspotify'
 
 require 'rails/all'
 
-RSpotify.authenticate("2bddb2de8f6b4b7792f99cdb8aecbe19", "a5f9a55ab0db42f68a3c02cdab76fb61")
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,9 +13,17 @@ module SpotifyPlaylister
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+RSpotify.authenticate(ENV["API_KEY"], ENV["API_SECRET"])
